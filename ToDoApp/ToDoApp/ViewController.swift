df939f1649642c2b186015f1a3f45918a1235fa5
jun.kohda <>
2023-02-07 22:09:33 +0900

    メイン画面からストーリーボード除去

--- ToDoApp/ToDoApp/ViewController.swift
+++ ToDoApp/ToDoApp/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var label: UILabel = {
+        let label = UILabel(frame: CGRect(x: 0, y: UIScreen.main.bounds.height/2, width: UIScreen.main.bounds.width, height: 50))
+        label.textAlignment = .center
+        label.text = "No Storyboard!"
+        return label
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .lightGray
+        view.addSubview(label)
     }
 
 
